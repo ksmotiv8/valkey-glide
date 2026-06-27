67b355167f8b570e8815eda977cdab706f14ec4b
James Duong <duong.james@gmail.com>
2026-06-26 19:42:38 -0700

    chore: remove debug logging from jni_scope.rs
    Signed-off-by: James Duong <duong.james@gmail.com>

--- java/src/jni_scope.rs
+++ java/src/jni_scope.rs
@@ -86,6 +86,7 @@ pub extern "system" fn Java_glide_ffi_resolvers_GlideScopeResolver_glideScopeExe
 
             parent_id.and_then(|pid| client_registry.get(&pid).map(|e| e.value().clone()))
         };
+        };
 
         let mut args = args;
         let result =
